45e0dc68b0433341a7c9d60f3817104d907cd701
Palmer <reyflorian3@gmail.com>
2025-02-12 23:00:36 +0800

    SCRUM-49 fix product api

--- app/Http/Resources/StockMovementsResource.php
+++ app/Http/Resources/StockMovementsResource.php
@@ -9,19 +9,21 @@ use Illuminate\Http\Resources\Json\JsonResource;
 /** @mixin StockMovements */
 class StockMovementsResource extends JsonResource
 {
-	public function toArray(Request $request): array
-	{
-		return [
-			'id'            => $this->id,
-			'quantity'      => $this->quantity,
-			'movement_type' => $this->movement_type,
-			'description'   => $this->description,
-			'created_at'    => $this->created_at,
-			'updated_at'    => $this->updated_at,
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'            => $this->id,
+            'quantity'      => $this->quantity,
+            'movement_type' => $this->movement_type,
+            'description'   => $this->description,
+            'created_at'    => $this->created_at,
+            'updated_at'    => $this->updated_at,
 
-			'sku_id' => $this->sku_id,
+            'sku_id' => $this->sku_id,
 
-			'sku' => new SkuResource( $this->whenLoaded( 'sku' ) ),
-		];
-	}
+            'sku' => new SkuResource( $this->whenLoaded( 'sku' ) ),
+
+            'product' => new ProductResource( $this->whenLoaded( 'product' ) ),
+        ];
+    }
 }

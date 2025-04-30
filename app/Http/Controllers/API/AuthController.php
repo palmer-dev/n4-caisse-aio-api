1fff266633c74ac271db4892d6a1ebab03984988
Palmer <reyflorian3@gmail.com>
2025-04-30 23:57:44 +0200

    FEAT login route and me route

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AuthCredentialsRequest;
 use App\Http\Requests\AuthRequest;
+use Illuminate\Support\Facades\Auth;
 use Laravel\Sanctum\PersonalAccessToken;
 
 class AuthController extends Controller
@@ -24,5 +26,32 @@ class AuthController extends Controller
         return response()->json( [
             "data" => null
         ], 404 );
+    }
+
+    //
+    public function loginWithCredentials(AuthCredentialsRequest $request)
+    {
+        $loggedIn = Auth::attempt( ["email" => $request->validated( "email" ), "password" => $request->validated( "password" )], true );
+
+        if ($loggedIn) {
+            $user = Auth::user();
+            return response()->json( [
+                "data" => [
+                    "token" => $user->createToken( "mobile_app_token" )->plainTextToken,
+                    "user"  => $user
+                ]
+            ] );
+        }
+
+        return response()->json( [
+            "data" => null
+        ], 404 );
+    }
+
+    public function me()
+    {
+        return response()->json( [
+            "data" => Auth::user()
+        ] );
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,21 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SaleController;
 use Illuminate\Support\Facades\Route;
 
-Route::post( "/auth/login", [AuthController::class, "login"] );
+//Route::post( "/auth/login", [AuthController::class, "login"] );
+
+//Route::post( "/auth/login/credentials", [AuthController::class, "loginWithCredentials"] );
+
+Route::name("auth")->prefix("auth")->group(function () {
+    Route::post("/login/credentials", [AuthController::class, "loginWithCredentials"]);
+    Route::post("/login", [AuthController::class, "login"]);
+    Route::post("/logout", [AuthController::class, "logout"]);
+
+});
 
 Route::middleware( ["auth:sanctum"] )->group( function () {
+
+    Route::get("/auth/me", [AuthController::class, "me"]);
+
     Route::name( "employees." )->prefix( "employees" )->group( function () {
         Route::post( '/auth', [EmployeeController::class, "auth"] );
     } );

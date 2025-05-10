ee19eb663c4892f9d8263cf95d951eb504e38702
Palmer <reyflorian3@gmail.com>
2025-05-10 18:36:22 +0200

    FIX: API Sku return unit_amount_with_tax

--- app/Http/Resources/SkuResource.php
+++ app/Http/Resources/SkuResource.php
@@ -12,13 +12,14 @@ class SkuResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'            => $this->id,
-            'sku'           => $this->sku,
-            'name'          => $this->getComputedNameAttribute(),
-            'currency_code' => $this->currency_code,
-            'unit_amount'   => $this->unit_amount,
-            'created_at'    => $this->created_at,
-            'updated_at'    => $this->updated_at,
+            'id'                   => $this->id,
+            'sku'                  => $this->sku,
+            'name'                 => $this->getComputedNameAttribute(),
+            'currency_code'        => $this->currency_code,
+            'unit_amount'          => $this->unit_amount,
+            'unit_amount_with_tax' => $this->unit_amount * (1 + $this->product->vatRate->value / 100),
+            'created_at'           => $this->created_at,
+            'updated_at'           => $this->updated_at,
 
             'product_id' => $this->product_id,
 
